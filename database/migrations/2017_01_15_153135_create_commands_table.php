5da2cfb7ebdaa9d6d5960abd0eb2ec94269f72f3
Davide Tampellini <tampe125@gmail.com>
2017-01-15 17:03:09 +0100

    Added commands table

--- database/migrations/2017_01_15_153135_create_commands_table.php
+++ database/migrations/2017_01_15_153135_create_commands_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateClientidsTable extends Migration
+class CreateCommandsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,16 @@ class CreateClientidsTable extends Migration
      */
     public function up()
     {
-        Schema::create('clients', function (Blueprint $table) {
+        Schema::create('commands', function (Blueprint $table) {
             $table->increments('id');
             $table->string('client_id', 255);
+            $table->mediumText('command');
+            $table->string('module', 255);
+            $table->char('checksum', 40);
+            $table->longText('result');
+	        $table->dateTime('created_at');
+	        $table->dateTime('executed_at');
+	        $table->dateTime('response_at');
         });
     }
 
@@ -26,6 +33,6 @@ class CreateClientidsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('clients');
+        Schema::dropIfExists('commands');
     }
 }
