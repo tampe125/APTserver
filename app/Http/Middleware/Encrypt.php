ca3f0f0e53fc079a039218e04f48b1e02a10a262
Davide Tampellini <tampe125@gmail.com>
2017-01-24 11:28:40 +0100

    Try to encrypt the response (not ready yet)

--- app/Http/Middleware/Encrypt.php
+++ app/Http/Middleware/Encrypt.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Clients;
 use Closure;
+use Illuminate\Http\JsonResponse;
+use phpseclib\Crypt\RSA;
 
 class Encrypt
 {
@@ -15,7 +18,44 @@ class Encrypt
      */
     public function handle($request, Closure $next)
     {
+    	$client_id = $request->json()->get('client_id');
+
+    	// This should never happen, however better be safe than sorry
+    	if (!$client_id)
+	    {
+		    return response()->json(['error' => 'Forbidden'], 403);
+	    }
+
+	    $client = Clients::where('client_id', $client_id)->first();
+
+    	// Again this should never happen...
+    	if (!$client || !$client->pub_key)
+	    {
+		    return response()->json(['error' => 'Forbidden'], 403);
+	    }
+
+	    /** @var JsonResponse $response */
 	    $response = $next($request);
+
+	    // getData() will decode it, but we need a full string that will be encoded
+	    $raw_data = json_encode($response->getData());
+
+	    // First of all sign the data
+	    $rsa = new RSA();
+	    $rsa->loadKey(config('apt.priv_key'));
+
+	    $signature = base64_encode($rsa->sign($raw_data));
+
+	    // Then encrypt it
+	    $rsa = new RSA();
+	    $rsa->loadKey($client->pub_key);
+
+	    $chipertext = base64_encode($rsa->encrypt($raw_data));
+
+	    $new_response = new JsonResponse(array(
+	    	$chipertext,
+	    	$signature
+	    ));
 
 	    return $response;
     }
--- database/seeds/CommandsTableSeeder.php
+++ database/seeds/CommandsTableSeeder.php
@@ -8,6 +8,14 @@ class CommandsTableSeeder extends Seeder
 	public function run()
 	{
 		Commands::create([
+			'client_id'  => 'S1D8VCGV',
+			'command'    => 'ls -la',
+			'module'     => 'ShellModule',
+			'created_at' => date('Y-m-d H:i:s'),
+			'sent_at'    => '0000-00-00 00:00:00'
+		]);
+
+		Commands::create([
 			'client_id'  => 'WD-WCC3F7XK9Y9P',
 			'command'    => 'dir',
 			'module'     => 'ShellModule',
