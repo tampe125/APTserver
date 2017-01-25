9e33d30c801ef358b145f7fdb500936364de353d
Davide Tampellini <tampe125@gmail.com>
2017-01-25 20:11:30 +0100

    Use the client id from the request to fetch the AES key Decrypt the incoming request using AES

--- app/Http/Middleware/AESDecrypt.php
+++ app/Http/Middleware/AESDecrypt.php
@@ -4,9 +4,8 @@ namespace App\Http\Middleware;
 
 use App\Clients;
 use Closure;
-use Defuse\Crypto\Crypto;
-use Defuse\Crypto\Key;
 use phpseclib\Crypt\AES;
+use phpseclib\Crypt\RSA;
 
 class AESDecrypt
 {
@@ -19,23 +18,29 @@ class AESDecrypt
      */
     public function handle($request, Closure $next)
     {
+	    $priv_key = config('apt.priv_key');
 	    $data = $request->input();
 
 	    // If any required param is missing, stop here
-	    if (!is_array($data) || ! isset($data[0]) || !isset($data[1]))
+	    if (!is_array($data) || ! isset($data[0]) || !isset($data[1]) || !isset($data[2]))
 	    {
 		    return response()->json(['error' => 'Forbidden'], 403);
 	    }
 
 	    $signature = base64_decode($data[1]);
 	    $encrypted = base64_decode($data[0]);
+	    $client_id = base64_decode($data[2]);
 
-	    if (!$signature || !$encrypted)
+	    if (!$signature || !$encrypted || !$client_id)
 	    {
 		    return response()->json(['error' => 'Forbidden'], 403);
 	    }
 
-	    $client_id = 'WD-WCC3F7XK9Y9P';
+	    $rsa = new RSA();
+	    $rsa->loadKey($priv_key);
+
+	    $client_id = $rsa->decrypt($client_id);
+
 	    $client = Clients::where('client_id', $client_id)->first();
 	    $aes_key = base64_decode($client->aes_key);
 
@@ -43,11 +48,25 @@ class AESDecrypt
 	    $cipher = new AES();
 	    $cipher->setKey($aes_key);
 
-	    $iv = mb_substr($encrypted, 0, 16);
-	    $ciphertext = mb_substr($encrypted, 16);
-	    $cipher->setIV($iv);
+	    $plaintext = $cipher->decrypt($encrypted);
+	    $plaintext = substr($plaintext, 16);
+	    $decoded   = json_decode($plaintext, true);
 
-	    $x = $cipher->decrypt($ciphertext);
+	    // Check if the decoded data has all the things we need
+	    if (!$decoded || !isset($decoded['client_id']) || !$decoded['client_id'])
+	    {
+		    return response()->json(['error' => 'Forbidden'], 403);
+	    }
+
+	    if ($decoded['client_id'] != $client_id)
+	    {
+		    return response()->json(['error' => 'Forbidden'], 403);
+	    }
+
+	    foreach ($decoded as $key => $value)
+	    {
+		    $request->json()->set($key, $value);
+	    }
 
         return $next($request);
     }
